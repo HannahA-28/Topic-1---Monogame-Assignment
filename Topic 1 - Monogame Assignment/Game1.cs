391bd9e8cce52a4436f26b5df26b2f32903c6543
HannahA-28 <339729345@2260I-L29-031>
2026-04-01 13:18:51 -0400

    need to get a font

--- Topic 1 - Monogame Assignment/Game1.cs
+++ Topic 1 - Monogame Assignment/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Reflection.Emit;
 
 namespace Topic_1___Monogame_Assignment
 {
@@ -8,6 +9,25 @@ namespace Topic_1___Monogame_Assignment
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+        Texture2D backgroundTexture;
+
+        Texture2D cowTexture;
+        Rectangle cowRect;
+
+        Texture2D horseTexture;
+        Rectangle horseRect;
+
+        Texture2D pigTexture;
+        Rectangle pigRect;
+
+        Texture2D chickenTexture;
+        Rectangle chickenRect;
+
+        Texture2D sheepTexture;
+        Rectangle sheepRect;
+
 
         public Game1()
         {
@@ -19,6 +39,17 @@ namespace Topic_1___Monogame_Assignment
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            Window.Title = "Farm";
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            cowRect = new Rectangle(300, 300, 100, 100);
+            horseRect = new Rectangle(0, 300, 150, 150);
+            pigRect = new Rectangle(700, 400, 100, 100);
+            chickenRect = new Rectangle(200, 480, 70, 70);
+            sheepRect = new Rectangle(550, 300, 100, 100);
 
             base.Initialize();
         }
@@ -28,6 +59,12 @@ namespace Topic_1___Monogame_Assignment
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            backgroundTexture = Content.Load<Texture2D>("farm");
+            cowTexture = Content.Load<Texture2D>("cow");
+            horseTexture = Content.Load<Texture2D>("horse");
+            pigTexture = Content.Load<Texture2D>("pig");
+            chickenTexture = Content.Load<Texture2D>("chicken");
+            sheepTexture = Content.Load<Texture2D>("sheep");
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +82,17 @@ namespace Topic_1___Monogame_Assignment
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(backgroundTexture, window, Color.White);
+            _spriteBatch.Draw(cowTexture, cowRect, Color.White);
+            _spriteBatch.Draw(horseTexture, horseRect, Color.White);
+            _spriteBatch.Draw(pigTexture, pigRect, Color.White);
+            _spriteBatch.Draw(chickenTexture, chickenRect, Color.White);
+            _spriteBatch.Draw(sheepTexture, sheepRect, Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
